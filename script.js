066e01ac6c654b5bcf3ed96e25f99ca3e83f2673
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-24 14:21:04 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -75,33 +75,3 @@ window.onclick = function (event) {
 };
 
 
-const selectors = [
-    '#linkedin img',
-    '#instagram img',
-    '#github img',
-    '#blog img',
-    '#Buyamecoffe img',
-    '#Localização img',
-    '#spotfy img',
-    '#projetos img',
-    '#curriculo img',
-    '#snakegame img',
-    '#certificados img',
-    '#bio img',
-    '#tech img',
-    '#produtividade img',
-    '#experiencias img',
-    '#cursos img'
-];
-
-function resetAnimation(element) {
-    element.classList.remove('animationClass');
-    setTimeout(function() {
-        element.classList.add('animationClass');
-    }, 10);
-}
-
-selectors.forEach(selector => {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach(resetAnimation);
-});
