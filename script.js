96fefbe0aff077b89482856bd8015cceb873a671
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-24 19:17:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
           body.classList.add("dark-mode");
         }
       }
-      body.style.transform = 'translateZ(0)';
+body.style.transform = 'translateZ(0)';
     });
   });
 });
